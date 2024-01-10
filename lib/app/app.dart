fd11abf9420dc1eeee5359cf42bdf28eb98faaf4
annisa <annisaputriwahyuni46@gmail.com>
2024-01-10 15:35:16 +0700

    chore: add hyperlink github

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:annisa_fb/ui_screens/home/home.view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
